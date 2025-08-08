64b9575eece57b2589c32f201460cf4f8e08a801
Angelo Orfanò <AngelOrfan96@gmail.com>
2025-08-08 16:46:39 +0200

    aumentata la velocità

--- minigame-cacciaTesoro.js
+++ minigame-cacciaTesoro.js
@@ -844,17 +844,18 @@ function hideTouchArrowsIfJoystick() {
 }
 hideTouchArrowsIfJoystick();
 
-console.log('SCRIPT CARICATO!');
-console.log('btn-minigame-treasure:', document.getElementById('btn-minigame-treasure'));
+window.addEventListener('DOMContentLoaded', function() {
+  // qui metti
+  console.log('SCRIPT CARICATO!');
+  console.log('btn-minigame-treasure:', document.getElementById('btn-minigame-treasure'));
 
-// ----- AVVIO DA BOTTONE -----
-document.getElementById('btn-minigame-treasure').addEventListener('click', () => {
-  document.getElementById('minigame-select-modal').classList.add('hidden');
-  document.getElementById('treasure-minigame-modal').classList.remove('hidden');
-  resizeTreasureCanvas();
-  startTreasureMinigame();
+  document.getElementById('btn-minigame-treasure').addEventListener('click', () => {
+    document.getElementById('minigame-select-modal').classList.add('hidden');
+    document.getElementById('treasure-minigame-modal').classList.remove('hidden');
+    resizeTreasureCanvas();
+    startTreasureMinigame();
+  });
+  document.getElementById('treasure-exit-btn').addEventListener('click', () => {
+    endTreasureMinigame();
+  });
 });
-document.getElementById('treasure-exit-btn').addEventListener('click', () => {
-  endTreasureMinigame();
-});
-

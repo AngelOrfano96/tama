70417c6bc12fbedc32461693e43469151f390ef3
Angelo Orfanò <AngelOrfan96@gmail.com>
2025-08-13 19:47:49 +0200

    aumentata la velocità

--- minigame-cacciaTesoro.js
+++ minigame-cacciaTesoro.js
@@ -290,10 +290,10 @@ G.sprites.wallParts = {
   corner_bl: loadImg(`${tileBase}/muroDungeon_angolosinistro_Basso.png`),
 
   // curve di chiusura ai bordi vicino alle porte (gli "angoli" che volevi usare)
-  angleTL:  loadImg(`${tileBase}/muroDungeon_curva_TL.png`),   // metti il nome giusto del tuo asset
-  angleTR:  loadImg(`${tileBase}/muroDungeon_curva_TR.png`),
-  angleBL:  loadImg(`${tileBase}/muroDungeon_curva_BL.png`),
-  angleBR:  loadImg(`${tileBase}/muroDungeon_curva_BR.png`),
+  angleTL:  loadImg(`${tileBase}/muroDungeon_angolosinistro_Alto.png`),   // metti il nome giusto del tuo asset
+  angleTR:  loadImg(`${tileBase}/muroDungeon_angolodestro_Alto.png`),
+  angleBL:  loadImg(`${tileBase}/muroDungeon_angolodestro_Basso.png`),
+  angleBR:  loadImg(`${tileBase}/muroDungeon_angolosinistro_Basso.png`),
 
   // lati (2 varianti per continuità)
   top:    [ loadImg(`${tileBase}/muroDungeon_Alto_1.png`),       loadImg(`${tileBase}/muroDungeon_Alto_2.png`) ],
